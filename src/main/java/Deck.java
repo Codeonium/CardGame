9ef4503147a135455a05b0d89eea98d0fd06e09a
codeonium <codeon2020@gmail.com>
2021-05-06 16:18:51 +0100

    end of lab, still plenty to work on

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -1,2 +1,49 @@
+import javax.smartcardio.Card;
+import java.util.ArrayList;
+
 public class Deck {
+
+    private ArrayList<Card> cards;
+
+    public Deck() {
+        this.cards = new ArrayList<>();
+    }
+
+    public int getDeckSize(){
+        return this.cards.size();
+    }
+
+    public void populateDeck() {
+    }
+
+
+//    public void loadDeck1() {
+//        cards = new ArrayList<Card>();
+//
+//        for(int i = 0; i < RankType.length; i++) {
+//            for(int j =0; j < SuitType.length; j++)
+//            {
+//            cards.add(new Card(RankType[i], SuitType[j]));
+//            }
+//        }
+//    }
+
+    public void fill() {
+        int i = 0;
+        for (SuitType s : SuitType.values()) {
+            for (RankType r : RankType.values()) {
+                Card cards[] = new ArrayList<Card>(r,s);
+                i++;
+            }
+        }
+    }
+
+    // populateDeck() -> void
+    // Enums - for suites and for ranks
+    // loop through all suites
+    // loop through all ranks
+    // create a card object with the currently iterated
+    // suite and rank
+    //add said card to this.cards
+
 }
--- src/test/java/DeckTest.java
+++ src/test/java/DeckTest.java
@@ -1,2 +1,43 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class DeckTest {
+
+    Deck deck;
+
+    @Before
+    public void before() {
+        deck = new Deck();
+    }
+
+    @Test
+    public void DeckStartsEmpty() {
+        assertEquals(0, deck.getDeckSize());
+    }
+
+
+
+    @Test
+    public void canPopulateDeck() {
+        deck.populateDeck();
+        assertEquals(52,deck.getDeckSize());
+    }
+    @Test
+    public void canPopulateDeck1() {
+        deck.loadDeck1();
+        assertEquals(52,deck.getDeckSize());
+    }
+    @Test
+    public void canPopulateDeck2() {
+        deck.fill();
+        assertEquals(52,deck.getDeckSize());
+    }
+
 }
+
+
+//EachCardOnlyHasOneRankAndOneSuit
+// CardsDoNotRepeatThemselves
+//DeckHasXCards
